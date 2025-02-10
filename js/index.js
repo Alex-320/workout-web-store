f7af7db64669e21e04fb6be13990903e7878f68c
Alex-320 <olekmytnyk@gmail.com>
2025-02-10 02:34:46 +0100

    End an index.html page

---
+++ js/index.js
@@ -0,0 +1,17 @@
+function init() {
+  import("./index.collections.js");
+  import("./index.limited-offer.js");
+  import("./index.new-arrivals.js");
+  import("./index.carousel.js");
+  import("./index.products-list.js");
+}
+
+const totalPartials = document.querySelectorAll(
+  '[hx-trigger="load"], [data-hx-trigger="load"]'
+).length;
+let loadedPartialsCount = 0;
+
+document.body.addEventListener("htmx:afterOnLoad", () => {
+  loadedPartialsCount++;
+  if (loadedPartialsCount === totalPartials) init();
+});
